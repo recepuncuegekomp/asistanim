5dabffbabcca0fcdecec24c5c1dbb2886f54a07c
recepuncuegekomp <46439223+recepuncuegekomp@users.noreply.github.com>
2019-02-05 13:31:09 +0300

    Update index.php

--- index.php
+++ index.php
@@ -14,40 +14,7 @@ use Dialogflow\Action\Questions\Carousel\Option;
 
 $agent = new WebhookClient(json_decode(file_get_contents('php://input'),true));
 
-$conv->ask('Please choose below');
+$agent->reply('Hi, how can I help?');
 
-$conv->ask(
-    Carousel::create()
-    ->Option(
-        Option::create()
-        ->key('OPTION_1')
-        ->title('Option 1')
-        ->synonyms(['option one', 'one'])
-        ->description('Select option 1')
-        ->image('https://picsum.photos/300/300')
-    )
-    ->Option(
-        Option::create()
-        ->key('OPTION_2')
-        ->title('Option 2')
-        ->synonyms(['option two', 'two'])
-        ->description('Select option 2')
-        ->image('https://picsum.photos/300/300')
-    )
-    ->Option(
-        Option::create()
-        ->key('OPTION_3')
-        ->title('Option 3')
-        ->synonyms(['option three', 'three'])
-        ->description('Select option 3')
-        ->image('https://picsum.photos/300/300')
-    )
-    ->Option(
-        Option::create()
-        ->key('OPTION_4')
-        ->title('Option 4')
-        ->synonyms(['option four', 'four'])
-        ->description('Select option 4')
-        ->image('https://picsum.photos/300/300')
-    )
-);
+header('Content-type: application/json');
+echo json_encode($agent->render());
