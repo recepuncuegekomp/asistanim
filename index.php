86b81fdc110683d297797d8e57b18bf0266fa0fb
recepuncuegekomp <46439223+recepuncuegekomp@users.noreply.github.com>
2019-02-06 11:39:17 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
-//error_reporting(E_ALL);
-//ini_set('display_errors', 1);
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
 require 'vendor/autoload.php';
 
@@ -10,8 +10,9 @@ use Dialogflow\WebhookClient;
 $agent = new WebhookClient(json_decode(file_get_contents('php://input'),true));
 
 $text = \Dialogflow\RichMessage\Text::create()
-    ->text('Bu mesal text')
+    ->text('Bu mesaj text')
     ->ssml('<speak>Bu mesaj <say-as interpret-as="characters">ssml</say-as></speak>');
+	
 $agent->reply($text);
 
 header('Content-type: application/json');
