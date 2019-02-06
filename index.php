fa8a5ff72db853c665adbea3dd40c2009d90533b
recepuncuegekomp <46439223+recepuncuegekomp@users.noreply.github.com>
2019-02-06 11:35:13 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,19 @@
 <?php
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+//error_reporting(E_ALL);
+//ini_set('display_errors', 1);
 
 require 'vendor/autoload.php';
 
 use Dialogflow\WebhookClient;
-use Dialogflow\Action\Responses\SimpleResponse;
 
 $agent = new WebhookClient(json_decode(file_get_contents('php://input'),true));
 
-$agent->reply('İşte bu ayki kazancınız...');
+$text = \Dialogflow\RichMessage\Text::create()
+    ->text('Bu mesal text')
+    ->ssml('<speak>Bu mesaj <say-as interpret-as="characters">ssml</say-as></speak>')
+;
+$agent->reply($text);
 
 header('Content-type: application/json');
 echo json_encode($agent->render());
