daf76851deb7e51082245a073ffcfeb8d3855988
recepuncuegekomp <46439223+recepuncuegekomp@users.noreply.github.com>
2019-02-06 11:49:17 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,3 @@
-<?php
-
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
@@ -9,14 +7,22 @@ use Dialogflow\WebhookClient;
 
 $agent = new WebhookClient(json_decode(file_get_contents('php://input'),true));
 
+$conv = $agent->getActionConversation();
+
+if ($conv) {
+	$conv->close('Bu bir conversation işlemi.');
+} else {
+	$agent->reply('İşlem action conversation değil.');
+}
+
 /* $text = \Dialogflow\RichMessage\Text::create()
     ->text('Bu mesaj text')
     ->ssml('<speak>Bu mesaj <say-as interpret-as="characters">ssml</say-as></speak>');
 	
 $agent->reply($text); */
 
-$image = \Dialogflow\RichMessage\Image::create('https://picsum.photos/240/240');
-$agent->reply($image);
+/*$image = \Dialogflow\RichMessage\Image::create('https://picsum.photos/240/240');
+$agent->reply($image);*/
 
 header('Content-type: application/json');
 echo json_encode($agent->render());
