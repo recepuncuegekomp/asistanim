5b2b04bd52061dc7a868daf5376b9148a77defd5
recepuncuegekomp <46439223+recepuncuegekomp@users.noreply.github.com>
2019-02-06 11:41:52 +0300

    Update index.php

--- index.php
+++ index.php
@@ -9,11 +9,14 @@ use Dialogflow\WebhookClient;
 
 $agent = new WebhookClient(json_decode(file_get_contents('php://input'),true));
 
-$text = \Dialogflow\RichMessage\Text::create()
+/* $text = \Dialogflow\RichMessage\Text::create()
     ->text('Bu mesaj text')
     ->ssml('<speak>Bu mesaj <say-as interpret-as="characters">ssml</say-as></speak>');
 	
-$agent->reply($text);
+$agent->reply($text); */
+
+$image = \Dialogflow\RichMessage\Image::create('https://picsum.photos/240/240');
+$agent->reply($image);
 
 header('Content-type: application/json');
 echo json_encode($agent->render());
