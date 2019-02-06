c292538a02470f8cacfd6e5da470c87d7fc6d21d
recepuncuegekomp <46439223+recepuncuegekomp@users.noreply.github.com>
2019-02-06 18:02:57 +0300

    Update index.php

--- index.php
+++ index.php
@@ -14,11 +14,45 @@ use Dialogflow\Action\Questions\ListCard\Option;
 $agent = new WebhookClient(json_decode(file_get_contents('php://input'),true));
 $conv = $agent->getActionConversation();
 
+function getData( $rota ) {	
+	$data = [];	
+	$curl = curl_init();
+	curl_setopt_array($curl, array(
+	  CURLOPT_PORT => "81",
+	  CURLOPT_URL => 'http://egekomp.isimheryerde.com/Test/index.php?rota=' . $rota,
+	  CURLOPT_RETURNTRANSFER => true,
+	  CURLOPT_SSL_VERIFYPEER => false,
+	  CURLOPT_SSL_VERIFYHOST => 0,
+	  CURLOPT_ENCODING => "",
+	  CURLOPT_MAXREDIRS => 10,
+	  CURLOPT_TIMEOUT => 30,
+	  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	  CURLOPT_CUSTOMREQUEST => "POST",
+	  CURLOPT_POSTFIELDS => "",
+	  CURLOPT_HTTPHEADER => array(		
+		"cache-control: no-cache"
+	  ),
+	));
+	$response = curl_exec($curl);
+	$err = curl_error($curl);
+	curl_close($curl);
+	if ($err) {
+	  //echo "cURL Error #:" . $err;
+	} else {
+	  $data = json_decode($response);
+	}		
+	return $data;
+}
+
 if ($conv) {
 	/*$conv->close('Bu bir conversation işlemi.');*/	
 
-	$conv->ask('İşte rapor sonucunuz...');
-	$conv->close(Image::create('https://picsum.photos/240/240'));
+	$conv->ask('Rapor hazırlanıyor...');
+	
+	$data = getData('tahsildeki_cekler');
+	$conv->close( sprintf('Tasildeki çek adeti %s ve toplam tutar %s', $data['ADET'], $data['TUTAR']) );
+	
+	//$conv->close(Image::create('https://picsum.photos/240/240'));
 	
 	$agent->reply($conv);
 } else {
