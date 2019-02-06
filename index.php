b70a9abd2dc6c709082116381d5132b9551ebb83
recepuncuegekomp <46439223+recepuncuegekomp@users.noreply.github.com>
2019-02-06 11:37:32 +0300

    Update index.php

--- index.php
+++ index.php
@@ -11,8 +11,7 @@ $agent = new WebhookClient(json_decode(file_get_contents('php://input'),true));
 
 $text = \Dialogflow\RichMessage\Text::create()
     ->text('Bu mesal text')
-    ->ssml('<speak>Bu mesaj <say-as interpret-as="characters">ssml</say-as></speak>')
-;
+    ->ssml('<speak>Bu mesaj <say-as interpret-as="characters">ssml</say-as></speak>');
 $agent->reply($text);
 
 header('Content-type: application/json');
