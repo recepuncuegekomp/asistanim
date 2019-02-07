5371d5a60f0869149f56e6b9aecc0ea8eeb385dc
recepuncuegekomp <46439223+recepuncuegekomp@users.noreply.github.com>
2019-02-07 10:12:16 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,38 +3,50 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-function getCurl( $rota ) {	
-	$data = [];	
+require 'vendor/autoload.php';
+
+function TL($sayi) {
+	return number_format($sayi, 2, ',', '.');
+}
+
+function getData( $rota ) {	
+	$data = null;	
 	$curl = curl_init();
 	curl_setopt_array($curl, array(	  
 	  CURLOPT_URL => 'http://egekomp.isimheryerde.com/Test/index.php?rota=' . $rota,
-	  CURLOPT_RETURNTRANSFER => false,
+	  CURLOPT_RETURNTRANSFER => true,
 	  CURLOPT_SSL_VERIFYPEER => false,
-	  CURLOPT_SSL_VERIFYHOST => 0,	  
+	  CURLOPT_SSL_VERIFYHOST => 0,
+	  CURLOPT_ENCODING => "",
+	  CURLOPT_MAXREDIRS => 0,
 	  CURLOPT_TIMEOUT => 90,
 	  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
 	  CURLOPT_CUSTOMREQUEST => "POST",
 	  CURLOPT_POSTFIELDS => "",
 	  CURLOPT_HTTPHEADER => array("cache-control: no-cache"),
-	));	
-	return $curl;
-}
-
-function tahsildeki_cekler( $rota ) {	
-	$data = [];		
-	$curl = getCurl( $rota );	
+	));
 	$response = curl_exec($curl);
 	$err = curl_error($curl);
 	curl_close($curl);
-	if ($err) {
-	  //echo "cURL Error #:" . $err;
-	} else {
+	if (!$err) {	
 	  $data = json_decode($response, true);
 	}		
-	return sprintf('Tahsildeki çek adeti %s ve toplam tutar %s', $data['ADET'], number_format($data['TUTAR'], 2, ',', '.'));
+	return $data;
 }
 
-require 'vendor/autoload.php';
+function getMesaj( $rota ) {		
+	$data = getData( $rota );	
+	$mesaj = "Sonuç bulunamadı...";
+	switch ($rota) {
+		case "tahsildeki_cekler":
+			$mesaj = sprintf('Tahsildeki çek adeti %s, toplam tutar ise %s', $data['ADET'], TL($data['TUTAR']));
+			break;
+		case "cari_borc_alacak":
+			$mesaj = sprintf('%s adet cari için, %s borç, %s alacak bulunuyor. Bakiye %s', $data['CARI_ADET'], TL($data['BORC']), TL($data['ALACAK']), TL($data['BAKIYE']));			
+			break;
+	}
+	return $mesaj;
+}
 
 use Dialogflow\WebhookClient;
 use Dialogflow\Action\Responses\Image;
@@ -49,8 +61,8 @@ $conv = $agent->getActionConversation();
 if ($conv) {
 	/*$conv->close('Bu bir conversation işlemi.');*/	
 
-	$data = call_user_func('tahsildeki_cekler');
-	$conv->ask( $data );
+	$sonuc = getMesaj('tahsildeki_cekler');
+	$conv->ask( $sonuc );
 	
 	//$conv->ask('İşte fotoğraf...');	
 	//$conv->close(Image::create('https://picsum.photos/240/240'));	
